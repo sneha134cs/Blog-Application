61420521315b39c216f7c87cd9048768cbdb8a03
snehamathew2002 <snehamathew2002@gmail.com>
2023-02-04 04:29:09 +0530

    connect MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 const express = require("express")
 const app = express()
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
 
+dotenv.config();
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(console.log("Connected to MongoDb"))
+.catch((err) => console.log(err));
 app.listen("5000",()=>{
     console.log("Backend is running")
 });
