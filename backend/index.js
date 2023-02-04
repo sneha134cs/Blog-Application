339b87c3a303236791ffd849d11c92ab306946db
Varun Pradeep <varunpradeep30@gmail.com>
2023-02-04 09:49:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,18 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 dotenv.config();
 
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(console.log("Connected to MongoDb"))
-.catch((err) => console.log(err));
-app.listen(process.env.PORT,()=>{
-    console.log("Backend is running")
+mongoose.set("strictQuery", false);
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(console.log("Connected to MongoDb"))
+	.catch((err) => console.log(err));
+app.listen(process.env.PORT, () => {
+	console.log("Backend is running");
 });
