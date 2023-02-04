ecf1dd886502fba1481224fa41bed876a3cd9827
Sneha S John <65975385+sneha134cs@users.noreply.github.com>
2023-02-04 19:19:14 +0530

    1

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,37 +1,61 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
+//UPDATE
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(401).json("You can update only on your account!");
+  }
+});
 
-router.post("/register", async (req, res) => {
+//DELETE
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted..");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json("User not found!");
+    }
+  } else {
+    res.status(401).json("You can delete only on your account!");
+  }
+});
+
+//GET USER
+router.get("/:id", async (req, res) => {
   try {
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPass = await bcrypt.hash(req.body.password,salt);
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPass,
-    });
-    const user = await newUser.save();
-    res.status(200).json(user);
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
   }
 });
-//LOGIN
-router.post("/login",async(req,res)=>{
-  try{
-      const user = await User.findOne({username: req.body.username})
-      !user && res.status(400).json("Wrong Credentials!")
 
-      const validated = await bcrypt.compare(req.body.password,user.password)
-      !validated && res.status(400).json("Wrong Credentials!")
-
-      const{password, ...others } = user._doc;
-      res.status(200).json(others);
-    }catch(err){
-    res.status(500).json(err);
-  }
-});
-module.exports = router
+module.exports = router;
