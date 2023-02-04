fba28e32340cf77732e687472ad2f473e9224d7f
Varun Pradeep <varunpradeep30@gmail.com>
2023-02-04 11:06:40 +0530

    call TopBar component in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
+import TopBar from "./components/topbar/TopBar";
 
 function App() {
-  return (
-    <div className="App">
-      blog app
-    </div>
-  );
+	return (
+		<>
+			<div className="App">
+				<TopBar />
+			</div>
+		</>
+	);
 }
 
 export default App;
