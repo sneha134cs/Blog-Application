72ff66d2a604597a4a35932904526967d7a73e62
snehamathew2002 <snehamathew2002@gmail.com>
2023-02-04 20:10:44 +0530

    POST CRUD

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 app.use(express.json());
@@ -18,6 +19,7 @@ mongoose
 
   app.use("/api/auth",authRoute);
   app.use("/api/users",userRoute);
+  app.use("/api/posts",postRoute);
 
   app.listen(process.env.PORT, () => {
   console.log("Backend is running");
