7bac349dc3d678f87d36bc8d6db2dac68af42e8a
Sneha S John <65975385+sneha134cs@users.noreply.github.com>
2023-02-04 17:23:28 +0530

    auth.js

---
+++ backend/routes/auth.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const User = require("../models/Users");
+
+router.post("/register", async (req, res) => {
+  try {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
