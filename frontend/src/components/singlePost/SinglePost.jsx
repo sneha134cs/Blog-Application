4a9b314f7731dd9983a28f062448d87706dc90d4
ShreyaBaburaj <shreyababuraj@gmail.com>
2023-02-04 16:56:34 +0530

    Update SinglePost.jsx

--- frontend/src/components/singlePost/SinglePost.jsx
+++ frontend/src/components/singlePost/SinglePost.jsx
@@ -3,7 +3,14 @@ import "./singlePost.css"
 export default function SinglePost(){
   return(
     <div className="singlePost">
-        single post
+        <div className="singlePostWrapper">
+          <img
+            src="https://i.pinimg.com/originals/d3/45/11/d3451114ab4e1d55ead624930bcff60c.jpg" 
+            alt="" 
+            className="singlePostImg"
+          />
+          <h1 className="singlePostTitle">Hello world</h1>
+        </div>
     </div>
   )
 }
