46fde8962232b0ed59b7642a1eadb572b9b69edc
ShreyaBaburaj <shreyababuraj@gmail.com>
2023-02-04 18:04:29 +0530

    single post

--- frontend/src/components/singlePost/SinglePost.jsx
+++ frontend/src/components/singlePost/SinglePost.jsx
@@ -9,7 +9,36 @@ export default function SinglePost(){
             alt="" 
             className="singlePostImg"
           />
-          <h1 className="singlePostTitle">Hello world</h1>
+          <h1 className="singlePostTitle">
+            Hello world
+            <div className="singlePostEdit">
+              <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
+              <i className="singlePostIcon fa-solid fa-trash"></i>
+            </div>
+          </h1>
+          <div className="singlePostInfo">
+              <span className="singlePostAuthor">Author: <b>Helen</b>      </span>
+              <span className="singlePostDate">   1 hour ago</span>
+          </div>
+          <p className="singlePostDesc">
+            description to be entered
+            description to be entered
+            description to be entered
+            description to be entered
+            description to be entered
+            description to be entered
+            description to be entered
+            description to be entered
+
+            description to be entered
+            description to be entered
+            description to be entered
+            description to be entered
+            description to be entered
+            description to be entered
+            description to be entered
+            description to be entered
+          </p>
         </div>
     </div>
   )
