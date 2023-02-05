f42c605d312d1b2602bc240f3859e7f44bcf6907
Varun Pradeep <varunpradeep30@gmail.com>
2023-02-05 10:42:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import TopBar from "./components/topbar/TopBar";
-//import Single from "./pages/single/Single";
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-
 
 function App() {
 	return (
 		<>
 			<div className="App">
 				<TopBar />
-			    <Write/>
+				<Write />
 			</div>
 		</>
 	);
