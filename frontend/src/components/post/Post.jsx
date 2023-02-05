b2c7b53e8e1488901923e802f62bce921ce68b43
Varun Pradeep <varunpradeep30@gmail.com>
2023-02-05 16:45:57 +0530

    connect post component

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,42 +1,26 @@
+import { Link } from "react-router-dom";
 import "./post.css";
 
-export default function Post() {
+export default function Post({ post }) {
 	return (
 		<div className="post">
-			<img
-				className="postImg"
-				src="https://images.pexels.com/photos/1083515/pexels-photo-1083515.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-				alt=""
-			/>
+			{post.photo && <img className="postImg" src={post.photo} alt="" />}
 			<div className="postInfo">
 				<div className="postCats">
-					<span className="postCat">Music</span>
-					<span className="postCat">Life</span>
+					{post.categories.map((category) => (
+						<span className="postCat">{category.name}</span>
+					))}
 				</div>
-				<span className="postTitle">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				</span>
+				<Link className="link" to={`/post/${post._id}`}>
+					<span className="postTitle">{post.title}</span>
+				</Link>
+
 				<hr />
-				<span className="postDate">1 hour ago</span>
+				<span className="postDate">
+					{new Date(post.createdAt).toDateString()}
+				</span>
 			</div>
-			<p className="postDesc">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Provident, suscipit eaque cumque adipisci ducimus eos saepe
-				eveniet molestiae vero nulla nobis. Corrupti praesentium quas
-				numquam! Voluptatem molestias nam omnis obcaecati. Lorem ipsum
-				dolor sit amet consectetur adipisicing elit. Provident, suscipit
-				eaque cumque adipisci ducimus eos saepe eveniet molestiae vero
-				nulla nobis. Corrupti praesentium quas numquam! Voluptatem
-				molestias nam omnis obcaecati. Lorem ipsum dolor sit amet
-				consectetur adipisicing elit. Provident, suscipit eaque cumque
-				adipisci ducimus eos saepe eveniet molestiae vero nulla nobis.
-				Corrupti praesentium quas numquam! Voluptatem molestias nam
-				omnis obcaecati. Lorem ipsum dolor sit amet consectetur
-				adipisicing elit. Provident, suscipit eaque cumque adipisci
-				ducimus eos saepe eveniet molestiae vero nulla nobis. Corrupti
-				praesentium quas numquam! Voluptatem molestias nam omnis
-				obcaecati.
-			</p>
+			<p className="postDesc">{post.desc}</p>
 		</div>
 	);
 }
