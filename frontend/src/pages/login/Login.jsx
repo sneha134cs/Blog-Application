ec6e39924f775e8af9133b535b00fb3ac5afbcf3
Varun Pradeep <varunpradeep30@gmail.com>
2023-02-05 11:51:13 +0530

    add login and register pages

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,25 @@
+import "./login.css";
+
+export default function Login() {
+	return (
+		<div className="login">
+			<span className="loginTitle">Login</span>
+			<form className="loginForm">
+				<label>Email</label>
+				<input
+					className="loginInput"
+					type="text"
+					placeholder="Enter your email..."
+				/>
+				<label>Password</label>
+				<input
+					className="loginInput"
+					type="password"
+					placeholder="Enter your email..."
+				/>
+				<button className="loginButton">Login</button>
+			</form>
+			<button className="loginRegisterButton">Register</button>
+		</div>
+	);
+}
