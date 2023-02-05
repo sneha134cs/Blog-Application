0508834f476163ab11855a7e86d5cf5dbf22faab
Varun Pradeep <varunpradeep30@gmail.com>
2023-02-05 13:12:51 +0530

    format code backend

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema(
-  {
-    name:{
-        type : String,
-        required: true
-    }
-  },
-  { timestamps: true }
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model("Category", CategorySchema);
