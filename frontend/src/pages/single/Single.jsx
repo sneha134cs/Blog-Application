e8c4b7cbac690870f354489e0d547f2c6292249b
Varun Pradeep <varunpradeep30@gmail.com>
2023-02-05 10:05:39 +0530

    add single page

--- frontend/src/pages/single/Single.jsx
+++ frontend/src/pages/single/Single.jsx
@@ -1,11 +1,12 @@
 import "./single.css";
 import SideBar from "../../components/sidebar/SideBar";
 import SinglePost from "../../components/singlePost/SinglePost";
+
 export default function single() {
-  return (
-    <div className="single">
-        <SinglePost/>
-        <SideBar/>
-    </div>
-  )
+	return (
+		<div className="single">
+			<SinglePost />
+			<SideBar />
+		</div>
+	);
 }
