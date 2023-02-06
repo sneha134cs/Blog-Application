c8738d2e654c9c7e52a048a566b5e4927ad25611
Varun Pradeep <varunpradeep30@gmail.com>
2023-02-07 03:42:23 +0530

    Update Categories.jsx

--- frontend/src/components/categories/Categories.jsx
+++ frontend/src/components/categories/Categories.jsx
@@ -1,17 +1,14 @@
 import "./categories.css";
 import { useState } from "react";
 import Category from "../category/Category";
-import SelectCategory from "../selectCategory/SelectCategory";
 
 export default function Categories({ post, mode }) {
 	return (
 		<div className="categories">
-			{categories.map((category) => (
-				<div className="category">
-					<span className="name">{category}</span>
-					<i class="categoryRemove fa-solid fa-xmark"></i>
-				</div>
-			))}
+			{post.categories &&
+				post.categories.map((category) => (
+					<Category category={category} mode={mode} />
+				))}
 		</div>
 	);
 }
